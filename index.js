b0db894365019055e2055e8aa7728dcda2f5c7f4
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 19:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for(var i = 0; i < musicians.length; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles;
+}
