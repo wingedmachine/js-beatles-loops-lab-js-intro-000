308ec2442731d157942f9290b905bd0b69fa89a3
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 19:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var i = 0;
-  while(i > facts.length) {
+  while(i++ > facts.length) {
     facts[i] = facts[i] + "!!!";
   }
   return facts;
